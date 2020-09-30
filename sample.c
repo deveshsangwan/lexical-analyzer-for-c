896f25a628bac202e2ddb57825ebd5ddbb475e59
Devesh Sangwan <dev.sangwan2001@gmail.com>
2020-09-30 21:46:50 +0530

    updated files

--- sample.c
+++ sample.c
@@ -4,6 +4,12 @@ int main(){
     int a = 10;
     int b = 0;
     //this is a comment
+
+
+
+
+
+
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
     /*
     this
@@ -11,6 +17,9 @@ int main(){
     a
     comment
     */
+
+
+   
     for (int i = 0; i < 8; i++)
     {
         b += arr[i];
